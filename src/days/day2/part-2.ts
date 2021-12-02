25a962ac179a2cc942ee0481cdf8ae9e8bed6dfa
Gabriel Théron <gabriel.theron@spendesk.com>
2021-12-02 12:27:06 +0100

    Crack day 2 part 2

---
+++ src/days/day2/part-2.ts
@@ -0,0 +1,88 @@
+import { flow, pipe } from "fp-ts/lib/function";
+import {
+  either as E,
+  array as A,
+  taskEither as TE,
+  task as T,
+  option as O,
+  nonEmptyArray as NEA,
+} from "fp-ts";
+import {
+  logAndPass,
+  getFileContents,
+  printError,
+  printResult,
+  splitIntoLines,
+} from "../../util";
+import { join as joinPath } from "path";
+
+const rootDirectory = process.cwd();
+
+const emptyCoordinates: Coordinates = {
+  position: 0,
+  depth: 0,
+  aim: 0,
+};
+
+async function main() {
+  await pipe(
+    joinPath(rootDirectory, "./input/day-2"),
+    getFileContents,
+    TE.map(splitIntoLines),
+    TE.map(A.map(parseCommand)),
+    // Turning the TE<Option<X>[]> into a TE<X[]>
+    TE.chainW(
+      flow(
+        A.sequence(O.Applicative),
+        TE.fromOption(() => "Some commands could not be decoded")
+      )
+    ),
+    TE.map(A.reduce(emptyCoordinates, updateCoordinates)),
+    TE.map(({ depth, position }) => depth * position),
+    TE.fold(flow(printError, T.of), flow(printResult, T.of))
+  )();
+}
+
+main();
+
+type Command = {
+  tag: "forward" | "up" | "down";
+  amount: number;
+};
+
+type Coordinates = {
+  depth: number;
+  position: number;
+  aim: number;
+};
+
+function parseCommand(line: string): O.Option<Command> {
+  const [tag, stringAmount] = line.split(" ");
+
+  if (tag !== "forward" && tag !== "up" && tag !== "down") {
+    return O.none;
+  }
+
+  return O.some({
+    tag,
+    amount: parseInt(stringAmount),
+  });
+}
+
+function updateCoordinates(
+  { depth, position, aim }: Coordinates,
+  command: Command
+): Coordinates {
+  switch (command.tag) {
+    case "forward":
+      return {
+        aim,
+        position: position + command.amount,
+        depth: depth + aim * command.amount,
+      };
+    case "down":
+      return { position, depth, aim: aim + command.amount };
+    case "up":
+      return { position, depth, aim: aim - command.amount };
+  }
+}
